7392e85356f48ffa5cc1963e1a9091f8d855f309
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-02 16:49:04 -0600

    wrapping numbers

--- src/components/test.svelte
+++ src/components/test.svelte
@@ -1,77 +1,86 @@
 <script lang="ts">
-	export let source = `jon
-sup there is some other text here that I want to go across the lines and not word wrap another line etc boy!
-me
-fixin to do sumthin good`;
+	import { onMount } from 'svelte';
 
-	let numberOfLines = source.split('\n').length;
+	export let source = `jon\nsup there is some other text here that I want to go across the lines and not word wrap another line etc boy!\nme\nfixin to do sumthin good`;
 
-	const KeyUp = (event: Event) => {
-		const textarea = event.target as HTMLInputElement;
-		numberOfLines = textarea.value.split('\n').length;
-	};
+	let fontWidthElem: HTMLElement;
+	let fontWidth: number;
+	let charactersPerLine = 60;
+	$: areaWidth = charactersPerLine * fontWidth;
 
-	const KeyDown = (event: Event) => {
-		const textarea = event.target as HTMLInputElement;
+	onMount(() => {
+		fontWidth = fontWidthElem.getBoundingClientRect().width;
+	});
 
-		if ((event as KeyboardEvent).key === 'Tab') {
-			const start = textarea.selectionStart;
-			const end = textarea.selectionEnd;
-
-			if (start && end) {
-				textarea.value = textarea.value.substring(0, start) + '\t' + textarea.value.substring(end);
-				event.preventDefault();
+	function wrapped(str: string, timesWrapped: number): number {
+		if (str.length > charactersPerLine) {
+			const firstLine = str.substring(0, charactersPerLine);
+			const lastCharOfFirstLine = str[charactersPerLine - 1];
+			const firstCharOfNextLine = str[charactersPerLine];
+			if (lastCharOfFirstLine !== ' ' && firstCharOfNextLine !== ' ') {
+				return wrapped(str.substring(firstLine.lastIndexOf(' ')).trim(), timesWrapped + 1);
+			} else {
+				return wrapped(str.substring(charactersPerLine).trim(), timesWrapped + 1);
 			}
+		} else {
+			return timesWrapped;
 		}
-	};
+	}
 </script>
 
-<center>
-	<div class="editor">
-		<div class="line-numbers">
-			{#each Array(numberOfLines) as _}
-				<span class="new-line" />
-			{/each}
-		</div>
-		<textarea class="text-content" on:keyup={KeyUp} on:keydown={KeyDown} value={source} rows="15" />
+<div class="editor">
+	<div class="line-numbers">
+		{#each source.split('\n') as subStr, index}
+			<div class="line-nr" style:margin-bottom="calc(var(--line-height) * {wrapped(subStr, 0)})">
+				{index + 1}
+			</div>
+		{/each}
 	</div>
-</center>
+
+	<textarea bind:value={source} style:width="{areaWidth}px" rows="15" />
+
+	<div id="font-width" bind:this={fontWidthElem}>a</div>
+</div>
 
 <style>
+	:global(body) {
+		padding: 0;
+	}
+
 	.editor {
+		--line-height: 21px;
+		position: relative;
 		display: inline-flex;
 		gap: 10px;
+		padding: 20px 10px;
 		font-family: monospace;
-		line-height: 21px;
+		line-height: var(--line-height);
 		background: #282a3a;
 		border-radius: 2px;
-		padding: 20px 10px;
 	}
 
-	.line-numbers {
+	.line-nr {
 		width: 20px;
 		text-align: right;
-	}
-
-	.line-numbers .new-line {
-		counter-increment: linenumber;
-	}
-
-	.line-numbers .new-line::before {
-		content: counter(linenumber);
-		display: block;
 		color: #506882 !important;
 	}
 
-	.text-content {
-		line-height: 21px;
+	textarea {
+		line-height: var(--line-height);
 		overflow-y: hidden;
+		margin: 0;
 		padding: 0;
 		border: 0;
 		background: #282a3a;
 		color: #fff;
-		min-width: 500px;
 		outline: none;
 		resize: none;
+	}
+
+	#font-width {
+		position: absolute;
+		top: -1000px;
+		left: -1000px;
+		visibility: hidden;
 	}
 </style>
